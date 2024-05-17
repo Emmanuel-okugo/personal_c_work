2bb29c3015033e78b9bbc73645e27472de8718b0
okugo chimdindu emmanuel <okugoemma@gmail.com>
2024-05-17 00:35:32 +0000

    tasks on structures

--- c_in_21_days/strings_in_c/e6.c
+++ c_in_21_days/strings_in_c/e6.c
@@ -1,7 +1,8 @@
 /*Write a function that accepts two strings. count the number of characters in each, and return a pointer to the longer string */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+
 
 /**
  * main - entry point
@@ -9,48 +10,49 @@
  * Return: always 0 (success)
  */
 
-char *string1;
+char string1[20];
 
-char *string2;
+char string2[20];
 
-char *string3;
+char * string3;
 
 int total_str_1;
 
 int total_str_2;
 
-char return_pointer(char *string1, char *string2, int total_str_1, int total_str_2);
+char * return_pointer(char string1[20], char string2[20], int total_str_1, int total_str_2);
 
 int main(void)
 {
 	/* calling the function */
-	*string3 = return_pointer(string1, string2, 0, 0);
+	string3 = return_pointer(string1, string2, 0, 0);
 
-
-	/* printing the longest string */
-	puts(string3);
+/*  printing the string */
+	printf("longest string: %s\n", string3);
 
 	return(0);
 }
 
-char return_pointer(char *string1, char *string2, int total_str_1, int total_str_2)
+char * return_pointer(char string1[20], char string2[20], int total_str_1, int total_str_2)
 {
 
 	/* getting string */
 	printf("first string: ");
-        total_str_1 = scanf("%s", string1);
+        scanf("%s", string1);
         printf("second string: ");
-        total_str_2 = scanf("%s", string2);
+        scanf("%s", string2);
+
+
+	total_str_1 = strlen(string1);
+	total_str_2 = strlen(string2);
 	
-	
+
 	if (total_str_1 > total_str_2)
 	{
-		return (*string1);
+		return(string1);
 	}
 	else
 	{
-		return (*string2);
+		return(string2);
 	}
 }
-
-
